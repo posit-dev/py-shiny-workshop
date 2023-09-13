453c2d9c2b8c585ce4aafd71bda03a7a8cdef0f1
Gordon Shotwell <gordon.shotwell@posit.co>
2023-09-13 11:09:06 -0300

    Merge cleanup

--- apps/examples/1.0-basic-app/app.py
+++ apps/examples/1.0-basic-app/app.py
@@ -1,13 +1,18 @@
 from shiny import App, render, ui
 
 
-def my_slider(id, label):
-    return ui.input_slider(id, label + "Number", 0, 100, 20)
+app_ui = ui.page_fluid(
+    ui.h2("Hello Shiny!"),
+    ui.input_slider("n", "N", 0, 100, 20),
+    ui.output_text_verbatim("txt"),
+)
 
 
-numbers = ["n1", "n2", "n3", "n4", "n5", "n6"]
-labels = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth"]
+def server(input, output, session):
+    @output
+    @render.text
+    def txt():
+        return f"n*2 is {input.n() * 2}"
 
-app_ui = ui.page_fluid([my_slider(x, y) for x, y in zip(numbers, labels)])
 
-app = App(app_ui, None)
+app = App(app_ui, server)
