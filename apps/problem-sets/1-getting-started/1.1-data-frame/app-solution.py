7595cadd537ab48cb731d8e468a853a91e27639b
Gordon Shotwell <gordon.shotwell@posit.co>
2024-03-15 11:19:43 -0300

    Expressify getting started problems

--- apps/problem-sets/1-getting-started/1.1-data-frame/app-solution.py
+++ apps/problem-sets/1-getting-started/1.1-data-frame/app-solution.py
@@ -1,18 +1,11 @@
-from shiny import Inputs, Outputs, Session, App, ui, render
+from shiny.express import ui, render
 import pandas as pd
 from pathlib import Path
 
 infile = Path(__file__).parent / "penguins.csv"
 penguins = pd.read_csv(infile)
 
-app_ui = ui.page_fluid(ui.output_data_frame("penguins_df"))
 
-
-def server(input: Inputs, output: Outputs, session: Session):
-    @output
-    @render.data_frame
-    def penguins_df():
-        return penguins
-
-
-app = App(app_ui, server)
+@render.data_frame
+def penguins_df():
+    return penguins
