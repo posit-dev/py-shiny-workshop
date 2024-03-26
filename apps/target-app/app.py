cbbaf79dc1f6cccb53f2210a6d809650492b78eb
Gordon Shotwell <gordon.shotwell@posit.co>
2024-03-26 11:10:00 -0300

    Auto stash before merge of "main" and "origin/main"

--- apps/target-app/app.py
+++ apps/target-app/app.py
@@ -15,13 +15,41 @@ from shinywidgets import render_plotly
 file_path = Path(__file__).parent / "simulated-data.csv"
 
 ui.page_opts(title="Monitoring")
+df = pd.read_csv(file_path, dtype={"sub_account": str})
+df["date"] = pd.to_datetime(df["date"], errors="coerce")
 
 
-@reactive.file_reader(file_path, interval_secs=0.2)
-def df():
-    out = pd.read_csv(file_path)
-    out["date"] = pd.to_datetime(out["date"], errors="coerce")
+@reactive.calc
+def monitor_sampled_data() -> pd.DataFrame:
+    start_date, end_date = input.dates()
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    df_value = df
+    out = df_value[
+        (df_value["date"] > start_date) & (df_value["date"] <= end_date)
+    ].sample(n=input.sample(), replace=True)
     return out
+
+
+@reactive.calc()
+def monitor_filtered_data() -> pd.DataFrame:
+    sample_df = monitor_sampled_data()
+    sample_df = sample_df.loc[sample_df["account"] == input.account()]
+    return sample_df.reset_index(drop=True)
+
+
+@reactive.calc()
+def training_data():
+    return df[
+        (df["account"] == input.account()) & (df["sub_account"] == input.sub_account())
+    ]
+
+
+@reactive.effect
+@reactive.event(input.reset)
+def reset_vals():
+    ui.update_date_range("dates", start="2023-01-01", end="2023-04-01")
+    ui.update_numeric("sample", value=10000)
 
 
 with ui.sidebar():
@@ -36,6 +64,13 @@ with ui.sidebar():
             "Wolff Ltd",
         ],
     )
+
+    @render.express
+    def sub_selector():
+        choice_data = df[df["account"] == input.account()]
+        choices = choice_data["sub_account"].unique().tolist()
+        ui.input_select("sub_account", "Sub Account", choices=choices)
+
     with ui.panel_conditional("input.tabs !== 'Training Dashboard'"):
         ui.input_date_range(
             "dates",
@@ -44,33 +79,25 @@ with ui.sidebar():
             end="2023-04-01",
         )
         ui.input_numeric("sample", "Sample Size", value=10000, step=5000)
+        ui.input_action_button("reset", "Reset Values", class_="btn-primary")
 
 
-@reactive.Calc
-def sampled_data() -> pd.DataFrame:
-    start_date, end_date = input.dates()
-    start_date = pd.to_datetime(start_date)
-    end_date = pd.to_datetime(end_date)
-    df_value = df()
-    out = df_value[
-        (df_value["date"] > start_date) & (df_value["date"] <= end_date)
-    ].sample(n=input.sample(), replace=True)
-    return out
+with ui.navset_bar(id="tabs", title="Monitoring"):
+    with ui.nav_panel("Training Dashboard"):
+        with ui.layout_columns():
+            with ui.card():
+                ui.card_header("Model Metrics")
 
-
-@reactive.Calc()
-def filtered_data() -> pd.DataFrame:
-    sample_df = sampled_data()
-    sample_df = sample_df.loc[sample_df["account"] == input.account()]
-    return sample_df.reset_index(drop=True)
-
-
-ui.nav_spacer()
-
-with ui.nav_panel("Training Dashboard"):
-    with ui.layout_columns():
-        with ui.card():
-            ui.card_header("Model Metrics")
+                @render_plotly
+                def metric():
+                    if input.metric() == "ROC Curve":
+                        return plot_auc_curve(
+                            training_data(), "is_electronics", "training_score"
+                        )
+                    else:
+                        return plot_precision_recall_curve(
+                            training_data(), "is_electronics", "training_score"
+                        )
 
             @render_plotly
             def metric():
@@ -85,13 +112,9 @@ with ui.nav_panel("Training Dashboard"):
                         df_filtered, "is_electronics", "training_score"
                     )
 
-            ui.input_select(
-                "metric",
-                "Metric",
-                choices=["ROC Curve", "Precision-Recall"],
-            )
-        with ui.card():
-            ui.card_header("Training Scores")
+                @render_plotly
+                def score_dist():
+                    return plot_score_distribution(training_data())
 
             @render_plotly
             def score_dist():
@@ -108,7 +131,7 @@ with ui.nav_panel("Training Dashboard"):
             ):
                 fa.icon_svg("download")
 
-                @render.download(filename="scores_data.csv")
+                @render.download(filename=lambda: f"{input.account()}_scores.csv")
                 def download_data():
                     with io.BytesIO() as buf:
                         filtered_data().to_csv(buf, index=False)
